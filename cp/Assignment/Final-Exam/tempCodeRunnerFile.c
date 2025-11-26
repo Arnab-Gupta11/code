1f7c7f6110c7b6f7b4b9888b48efd43024993937
Arnab Gupta <arnabgupta1111@gmail.com>
2025-11-26 21:54:26 +0600

    add code

---
+++ cp/Assignment/Final-Exam/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+for (int i = 0; i < 5; i++) {
+    //     int stars = N;
+    //     int spaces = (topWidth - stars) / 2;
+
+    //     for (int s = 0; s < spaces; s++) printf(" ");
+    //     for (int st = 0; st < stars; st++) printf("*");
+    //     printf("\n");
+    // }
